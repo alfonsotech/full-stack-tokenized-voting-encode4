8e73ac79cdcd3d394e027f65292ab4828da495f3
Alfonso Tech <alfonsoteched@nym.hush.com>
2024-08-31 12:30:05 -0400

    Adds TokenMinter to front end

--- frontend/packages/nextjs/components/Token/TokenInfo.tsx
+++ frontend/packages/nextjs/components/Token/TokenInfo.tsx
@@ -1,5 +1,6 @@
 import TokenBalance from "./TokenBalance";
 import TokenName from "./TokenName";
+import TokenMinter from "./TokenMinter";
 
 function TokenInfo(params: { address: `0x${string}` }) {
   return (
@@ -8,6 +9,8 @@ function TokenInfo(params: { address: `0x${string}` }) {
         <h2 className="card-title">Testing useReadContract wagmi hook</h2>
         <TokenName></TokenName>
         <TokenBalance address={params.address}></TokenBalance>
+        <h1>Mint MyTokens</h1>
+        <TokenMinter />
       </div>
     </div>
   );
